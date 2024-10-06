8fd53a2749144d9fb241d78e2b078d93218edba8
Arta Seyedian <arta.seyedian@gmail.com>
2024-10-06 13:36:51 -0400

    adding picar code

--- iot-lab-2/frontend_tutorial/wifi_server.py
+++ iot-lab-2/frontend_tutorial/wifi_server.py
@@ -1,8 +1,76 @@
 import socket
 from datetime import datetime
+import picar_4wd as fc
+import sys
+import tty
+import termios
 
 HOST = "192.168.68.110"  # IP address of your Raspberry PI
 PORT = 65432          # Port to listen on (non-privileged ports are > 1023)
+
+power_val = 50
+invert_turns = True  # Flag to invert turn directions, True by default
+
+print("If you want to quit, please press q")
+print("Press 'i' to toggle turn inversion")
+
+def readchar():
+    fd = sys.stdin.fileno()
+    old_settings = termios.tcgetattr(fd)
+    try:
+        tty.setraw(sys.stdin.fileno())
+        ch = sys.stdin.read(1)
+    finally:
+        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
+    return ch
+
+def readkey(getchar_fn=None):
+    getchar = getchar_fn or readchar
+    c1 = getchar()
+    if ord(c1) != 0x1b:
+        return c1
+    c2 = getchar()
+    if ord(c2) != 0x5b:
+        return c1
+    c3 = getchar()
+    return chr(0x10 + ord(c3) - 65)
+
+def Keyboard_control():
+    global power_val, invert_turns
+    while True:
+        key = readkey()
+        if key == '6':
+            if power_val <= 90:
+                power_val += 10
+                print("power_val:", power_val)
+        elif key == '4':
+            if power_val >= 10:
+                power_val -= 10
+                print("power_val:", power_val)
+        
+        if key == 'w':
+            fc.forward(power_val)
+        elif key == 's':
+            fc.backward(power_val)
+        elif key == 'a':
+            if invert_turns:
+                fc.turn_right(power_val)
+            else:
+                fc.turn_left(power_val)
+        elif key == 'd':
+            if invert_turns:
+                fc.turn_left(power_val)
+            else:
+                fc.turn_right(power_val)
+        elif key == 'i':
+            invert_turns = not invert_turns
+            print("Turn inversion:", "ON" if invert_turns else "OFF")
+        else:
+            fc.stop()
+        
+        if key == 'q':
+            print("Quitting")
+            break
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
